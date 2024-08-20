b13e296660cf1d06aba750414d653887a306f782
awaisrahman12 <140633107+awaisrahman12@users.noreply.github.com>
2024-08-20 16:57:47 +0500

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: 'realestate-8f842.firebaseapp.com',
+  projectId: 'realestate-8f842',
+  storageBucket: 'realestate-8f842.appspot.com',
+  messagingSenderId: '1081788327977',
+  appId: '1:1081788327977:web:09ff768caa0e78adc39438',
 };
 
 // Initialize Firebase
